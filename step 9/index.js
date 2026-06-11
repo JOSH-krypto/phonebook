85bf6e7d9991861f815a5365496a50a8cccd1cbd
JOSH-krypto <joash5764@gmail.com>
2026-06-11 10:52:55 +0530

    bug fixing

--- step 9/index.js
+++ step 9/index.js
@@ -4,7 +4,7 @@ const cors =require('cors')
 const app=express()
 
 app.use(cors())
-app.use(express.static('dist'))
+
 // const requestLogger = (request, response, next) => {
 //   console.log('Method:', request.method)
 //   console.log('Path:  ', request.path)
@@ -105,7 +105,7 @@ app.post('/api/persons', (request, response) => {
 // }
 // app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT =  3001
 app.listen(PORT,()=>{
     console.log(`port ${PORT} running...`)
 })
