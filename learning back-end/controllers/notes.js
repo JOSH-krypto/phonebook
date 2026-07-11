dc84fe98180670fae9f5eeafff01a3b8b8fa4208
JOSH-krypto <joash5764@gmail.com>
2026-07-11 22:13:06 +0530

    testing

---
+++ learning back-end/controllers/notes.js
@@ -0,0 +1,61 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/notes')
+
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
+})
+
+notesRouter.get('/:id', async (request, response, next) => {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  
+})
+
+notesRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  const savedNote = await  note.save()
+  response.status(201).json(savedNote)
+})
+
+notesRouter.delete('/:id',async (request, response, next) => {
+    await Note.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } 
+)
+
+notesRouter.put('/:id', async (request, response, next) => {
+  const { content, important } = request.body
+
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(
+      request.params.id,
+      { content, important },
+      {
+        new: true,
+        runValidators: true,
+        context: 'query'
+      }
+    )
+
+    if (!updatedNote) {
+      return response.status(404).end()
+    }
+
+    response.json(updatedNote)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = notesRouter
